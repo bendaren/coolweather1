95ce780b2e0920337624cf4916d4c85161394f40
laoqi <648310412@qq.com>
2017-04-23 15:00:35 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/bendaren/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/bendaren/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.bendaren.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -64,7 +65,7 @@ public class ChooseAreaFragment extends Fragment {
         titleText = (TextView) view.findViewById(R.id.title_text);
         backButton = (Button) view.findViewById(R.id.back_button);
         listView = (ListView) view.findViewById(R.id.list_view);
-        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
         listView.setAdapter(adapter);
         return view;
     }
@@ -81,6 +82,20 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if(currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    if (getActivity() instanceof MainActivity){
+                        Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
---
+++ app/src/main/java/com/example/bendaren/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.bendaren.coolweather.gson;
+
+/**
+ * Created by lenovo on 2017/4/23.
+ */
+
+public class AQI {
+    public  AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
